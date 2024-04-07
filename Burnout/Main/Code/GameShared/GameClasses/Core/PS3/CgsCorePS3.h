7f43075d148bf93d1c42b09464637eb9a60690d3
Nathan V <jebcraftserver@gmail.com>
2024-04-07 03:27:28 -0400

    (Core) PS3 CgsCore MemCpy + MemSet extern decls

--- Burnout/Main/Code/GameShared/GameClasses/Core/PS3/CgsCorePS3.h
+++ Burnout/Main/Code/GameShared/GameClasses/Core/PS3/CgsCorePS3.h
@@ -3,5 +3,11 @@
 
 namespace CgsCore
 {
+	// Not sure if the param names are correct
+	// but still wanted to include the externs
+	extern void* MemSet(void* lpDest_, int iCh_, size_t liCount_);
+
+	extern void* MemCpy(void* lpDest_, const void* lpSrc_, size_t liCount_);
+
 	static const int32_t KI_CACHELINESIZE = KI_PPCCACHELINESIZE;
 }
